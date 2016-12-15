39f0ded32f9d156b3a5639fc52e6cdcb8b7f4f14
Carlos Casares <ccasares@gmail.com>
2016-12-15 12:33:21 +0100

    log

--- server.js
+++ server.js
@@ -233,7 +233,7 @@ router.post(URI, function(req, res) {
       res.status(500).send({ message: err });
     } else {
       // "command" object contains all data. Send it over WS
-      //console.log("%j", command);
+      console.log("%j", command);
       if ( ws) {
         ws.send(JSON.stringify(command));
         response = "Command sent successfully";
